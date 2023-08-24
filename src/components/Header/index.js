dc86300d81dfc587e39407b5da0679802c6e88fd
Maximo <maxmonteiro86@gmail.com>
2023-08-24 16:31:46 -0300

    cria estrutura e estilo do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,16 +3,24 @@ import {
     View, 
     StyleSheet,
     Text,
-    StatusBar
+    StatusBar,
+    TouchableOpacity
+} from 'react-native';
 
-} from 'react-native'
+import { Feather } from '@expo/vector-icons';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export default function Header() {
     return (
         <View style={styles.container}>
-            <Text>Header do app</Text>
+            <View style={styles.content}>
+                <Text style={styles.username}>Usuário logado</Text>
+
+                <TouchableOpacity activeOpacity={0.8} style={styles.buttonUser}>
+                    <Feather name="user" size={27} color="#FFF" />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -20,6 +28,32 @@ export default function Header() {
 const styles = StyleSheet.create({
     container: {
         paddingTop: statusBarHeight,
+        flexDirection: 'row',
+        paddingStart: 16,
+        paddingEnd: 16,
+        paddingBottom: 44,
         backgroundColor: '#8000ff'
+    },
+
+    content: {
+        flex: 1, // pegar o tamanho inteiro da tela
+        alignItems: 'center',
+        flexDirection: 'row', // elementos um ao lado do outro
+        justifyContent: 'space-between', // elementos nas extremidades
+    },
+
+    username: {
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
+
+    buttonUser: {
+        width: 44,
+        height: 44,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 44/2
     }
 })
