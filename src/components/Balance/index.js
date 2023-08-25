2d20043652ec9c8bfba30c37b4d1b22682bef320
Maximo <maxmonteiro86@gmail.com>
2023-08-25 08:45:35 -0300

    cria componente Balance

---
+++ src/components/Balance/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { 
+    View, 
+    Text,
+    StyleSheet,
+} from 'react-native'
+
+export default function Balance() {
+  return (
+    <View style={styles.container}>
+      <Text>BALANCE</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Header from '../../components/Header'
+import Balance from '../../components/Balance';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Usuário logado"/>
-      <Text>Página Home</Text>
+      <Balance />
     </View>
   );
 }
