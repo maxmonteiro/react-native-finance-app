4691c83d352f845bb4dee6b82fbdb21c3380e398
Maximo <maxmonteiro86@gmail.com>
2023-08-29 16:01:30 -0300

    cria componente de acoes Actions

---
+++ src/components/Actions/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { 
+    StyleSheet, 
+    Text, 
+    View 
+} from 'react-native'
+
+export default function Actions() {
+  return (
+    <View style={styles.container}>
+      <Text>Teste</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header'
 import Balance from '../../components/Balance';
 import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
 
 const list = [
   {
@@ -33,6 +34,8 @@ export default function Home() {
       <Header name="Usuário logado"/>
 
       <Balance saldo="9.250,00" gastos="-570,00"/>
+
+      <Actions />
 
       <Text style={styles.title}>Últimas movimentações</Text>
 
