4d7239eaa0838efce9aa43b0d48832705a5d0ff0
Maximo <maxmonteiro86@gmail.com>
2023-08-29 16:11:46 -0300

    cria estilo do componente de acoes

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -2,19 +2,84 @@ import React from 'react'
 import { 
     StyleSheet, 
     Text, 
-    View 
+    View,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native'
+
+import { AntDesign } from '@expo/vector-icons'
 
 export default function Actions() {
   return (
-    <View style={styles.container}>
-      <Text>Teste</Text>
-    </View>
+    <ScrollView style={styles.container}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+    >
+
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="addfolder" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Entradas</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="tagso" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Compras</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="creditcard" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Carteira</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="barcode" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Boleto</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="setting" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Conta</Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
+        maxHeight: 84,
+        marginTop: 18,
+        marginBottom: 14,
+        paddingEnd: 14,
+        paddingStart: 14,
+    },
 
+    actionButton: {
+        alignItems: 'center',
+        marginRight: 32,
+    },
+
+    areaButton: {
+        backgroundColor: '#ecf0f1',
+        width: 60,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30
+    },
+
+    labelButton: {
+        marginTop: 4,
+        textAlign: 'center',
+        fontWeight: 'bold'
     }
 })
