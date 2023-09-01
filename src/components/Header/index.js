bac1478836679b3bef4b6d5d65d2b5057e8b5232
Maximo <maxmonteiro86@gmail.com>
2023-09-01 15:22:27 -0300

    cria animacao para o header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,19 +8,48 @@ import {
 } from 'react-native';
 
 import { Feather } from '@expo/vector-icons';
+import { MotiView, MotiText } from 'moti';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export default function Header({ name }) {
     return (
         <View style={styles.container}>
-            <View style={styles.content}>
-                <Text style={styles.username}>{name}</Text>
+            <MotiView 
+                style={styles.content}
+                from={{
+                    translateY: -150,
+                    opacity: 0
+                }}
+                animate={{
+                    translateY: 0,
+                    opacity: 1
+                }}
+                transition={{
+                    type: "timing",
+                    duration: 800,
+                    delay: 300
+                }}
+            >
+                <MotiText 
+                    style={styles.username}
+                    from={{
+                        translateX: -300
+                    }}
+                    animate={{
+                        translateX: 0
+                    }}
+                    transition={{
+                        type: "timing",
+                        duration: 800,
+                        delay: 800
+                    }}
+                >{name}</MotiText>
 
                 <TouchableOpacity activeOpacity={0.8} style={styles.buttonUser}>
                     <Feather name="user" size={27} color="#FFF" />
                 </TouchableOpacity>
-            </View>
+            </MotiView>
         </View>
     )
 }
